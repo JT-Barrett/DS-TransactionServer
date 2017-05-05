3d69526e36ec29d517b3845950e4b25f8c9bd473
Jack "JT" Barrett <jtbarrett0804@hotmail.com>
2017-05-05 08:09:26 -0700

    Finished(?) Lock.java

--- Lock.java
+++ Lock.java
@@ -1,3 +1,6 @@
+// From Textbook, completed by JT Barrett and David Miller
+import java.io.*;
+
 public class Lock
 {
 	private Object object;
@@ -8,7 +11,8 @@ public class Lock
 	// the current type
 	public synchronized void acquire(TransID trans, LockType aLockType )
 	{
-		while(/*another transaction holds the lockin conflicting mode*/)
+		boolean notWaiting = true;
+		while(hasConflict(aLockType))
 		{
 			try
 			{
@@ -16,32 +20,68 @@ public class Lock
 			}
 			catch (InterruptedException e)
 			{
-				/*...*/
+				System.out.println("Caught Exception: " + e.getMessage());
+			}
+
+			if(waiting)
+			{
+				System.out.println("TransID: " + trans.id + " || waiting for lock access.")
+				waiting = false;
 			}
 		}
+		System.out.println("\n===========================\nTransID: " + trans.id + " || free to proceed.")
 		if (holders.isEmpty())
 		{
 			// no TIDs hold lock
 			holders.addElement(trans);
 			lockType  = aLockType;
+			System.out.println("TransID: " + trans.id + " || aquired " + aLockType.getType() + " lock: No other TIDs present.\n===========================\n")
 		}
-		else if (/*another transaction holds the lock, share it*/))
+		else if (!holders.isEmpty() && !isHolder(trans))
 		{
-			if (/* this transaction not a holder*/)
-			{
 				holders.addElement(trans);
-			}
+				System.out.println("TransID: " + trans.id + " || aquired " + aLockType.getType() + " lock: Lock shared.\n===========================\n")
 		}
-		else if (/* this transaction is a holder but needs amore exclusive lock*/)
+
+		/* this transaction is a holder but needs amore exclusive lock*/
+		else if (isHolder(trans) && aLockType.getType() == "WRITE")
 		{
 			lockType.promote();
+			System.out.println("TransID: " + trans.id + " || aquired " + aLockType.getType() + " lock: Lock promoted.\n===========================\n")
 		}
 	}
-	public synchronized void release(TransID trans )
+
+	public synchronized void release(TransID trans)
 	{
 		holders.removeElement(trans);
 		// remove this holder
 		// set locktype to none
 		notifyAll();
+	}
+
+	//Helper methods added in addition to book methods
+	public synchronized boolean isHolder (TransID trans){
+		for(int i = 0; i < holders.size(); i++ )
+		{
+			if (holders[i].id == trans.id)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public synchronized boolean hasConflict (LockType aLockType){
+		//if requesting read conflict if lock is WRITE
+		if ( aLockType.getType() == "READ" && lockType.getType() == "WRITE")
+		{
+				return true;
+		}
+		//if requesting WRITE, conflict there are lock of any kind
+		else if ( aLockType.getType() = "WRITE" && !holders.isEmpty()){
+				return true;
+		}
+
+		return false;
 	}
 }
