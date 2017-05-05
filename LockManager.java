266cf794c8f5cffb96eeb9774102b1545cea889f
Turtle Dynasty <dm667@nau.edu>
2017-05-04 23:34:47 -0700

    trying to write search function

--- LockManager.java
+++ LockManager.java
@@ -1,14 +1,24 @@
 // From Textbook, completed by JT Barrett and David Miller
+import java.lang.Integer;
+import java.util.Hashtable;
+
 public class LockManager
 {
-	private Hashtable theLocks;
-	public  void setLock(Object object, TransID trans,  LockType lockType)
+	private Hashtable <Integer, Lock> theLocks;
+
+	public void setLock(Object object, TransID trans, LockType lockType)
 	{
 		Lock foundLock;
 		synchronized(this)
 		{
-			// find the lock associated with object
+            // find the lock associated with object
+            foundLock = searchHashTableForLock(object);
+                
 			// if there isn’t one, create it and add it to the hashtable
+            if (foundLock == null) 
+            {
+                // create lock and add to hash table
+            }
 		}
 		foundLock.acquire(trans, lockType);
 	}
@@ -25,5 +35,19 @@ public class LockManager
 			}
 		}
 	}
+    
+    public Lock searchHashTableForLock(Object obj)
+    {
+        for (Object obj: theLocks.entrySet()){
+            // make a reference to the object as a map entry
+            Map.Entry entry = (Map.Entry) obj;
+            if(entry.getKey().equals(obj))
+            {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
 }
 
